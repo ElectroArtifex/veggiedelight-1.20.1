6fa8b3b60faa02c9dcb3fe666de3376d30816767
ElectroArtifex <ElectroArtifex@protonmail.com>
2023-11-23 22:40:20 -0600

    init

---
+++ src/main/java/net/electroartifex/veggiedelight/VeggieDelightClient.java
@@ -0,0 +1,10 @@
+package net.electroartifex.veggiedelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeggieDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
